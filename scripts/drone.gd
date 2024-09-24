635710e4da7bbbc1a18fe555c93d4b1772a6fa8e
Anas F <anas.fouad@etudiant.univ-brest.fr>
2024-09-24 11:18:34 +0200

    Before logic commit

--- scripts/drone.gd
+++ scripts/drone.gd
@@ -1,11 +1,15 @@
-extends Node3D
+extends RigidBody3D
 
 var speed: float = 2.0
 var angle: float = 0.0  # Angle pour le mouvement circulaire
 var radius: float = 5.0  # Rayon du cercle
 
-func _process(delta):
-	move_in_circle(delta)
+func _ready():
+	pass
+
+func _process(_delta):
+	pass
+	#move_in_circle(delta)
 
 func move_in_circle(delta):
 	# Calculer la nouvelle position en fonction de l'angle
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,32 +1,28 @@
 extends Node3D
 
 const drone: Resource = preload("res://scenes/drone.tscn")
+@onready var light = $DirectionalLight3D
+@onready var camera = $Camera3D
+@onready var drones = $Drones
 var time_since_last_add: float = 0.0
 var add_interval: float = 1.0  # Intervalle d'ajout en secondes
-var circle_radius: float = 10.0  # Rayon du cercle
-var num_drones: int = 1  # Nombre total de drones à instancier
+var circle_radius: float = 20.0  # Rayon du cercle
+var max_num_drones: int = 3  # Nombre total de drones à instancier
 
 func _ready():
-	# Ajouter une lumière
-	var light = DirectionalLight3D.new()
-	light.rotation_degrees = Vector3(45, 0, 0)
-	add_child(light)
-
-	# Ajouter une caméra
-	var camera = Camera3D.new()
-	camera.position = Vector3(0, 10, -20)
-	camera.look_at(Vector3.ZERO, Vector3.UP)
-	add_child(camera)
+	# Light properties
+	light.rotation_degrees = Vector3(-90, 0, 0)
+	light.position = Vector3(0, 10, 0)
 
 func _process(delta):
 	time_since_last_add += delta
 	if time_since_last_add >= add_interval:  # Ajouter un drone à chaque intervalle
-		if randf() < 0.1 and get_child_count() < num_drones:  # Ajouter un drone de temps en temps
+		if randf() < 0.1 and drones.get_child_count() < max_num_drones:  # Ajouter un drone de temps en temps
 			var d: Node3D = drone.instantiate()
 			add_child(d)  # Ajouter le drone à la scène
 
 			# Initialiser la position en cercle
-			var angle: float = (2.0 * PI / num_drones) * get_child_count()
+			var angle: float = (2.0 * PI / max_num_drones) * get_child_count()
 			d.position.x = circle_radius * cos(angle)
 			d.position.z = circle_radius * sin(angle)
 
